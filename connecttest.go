c596c1e21fd3bb60ebee23d04676b4bab8d2b13e
Ernesto Alejo <ernestokarim@gmail.com>
2025-02-22 17:33:29 +0100

    Require not nil errors.

--- connecttest.go
+++ connecttest.go
@@ -10,6 +10,7 @@ import (
 
 // RequireError assets that the Connect error is the expected one.
 func RequireError(t *testing.T, err error, code connect.Code, message string) {
+	require.Error(t, err)
 	if connecterr := new(connect.Error); errors.As(err, &connecterr) {
 		require.Equal(t, connecterr.Code(), code)
 		require.Equal(t, connecterr.Message(), message)
